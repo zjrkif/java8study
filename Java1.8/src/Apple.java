de2ecc01a21ecfb2bc30b4dbb4e4bc69ab6fe3c5
zjrkif <231144782@qq.com>
2017-12-06 13:21:44 +0800

    first

---
+++ Java1.8/src/Apple.java
@@ -0,0 +1,28 @@
+
+public class Apple {
+	private int weight;
+	
+	private String color;
+	
+	public Apple(int weight,String color){
+		this.weight=weight;
+		this.color=color;
+	}
+	
+	public String toString(){
+		return "[weight="+weight+",color="+color+"]";
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
---
+++ Java1.8/src/Test.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Test {
+	public static void main(String[] args) {
+		List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"));
+
+		// [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+		List<Apple> greenApples = filterApples(inventory, Test::isGreenApple);
+		System.out.println(greenApples);
+
+		// [Apple{color='green', weight=155}]
+		List<Apple> heavyApples = filterApples(inventory, Test::isHeavyApple);
+		System.out.println(heavyApples);
+
+		// [Apple{color='green', weight=80}, Apple{color='green', weight=155}]
+		List<Apple> greenApples2 = filterApples(inventory, (Apple a) -> "green".equals(a.getColor()));
+		System.out.println(greenApples2);
+
+		// [Apple{color='green', weight=155}]
+		List<Apple> heavyApples2 = filterApples(inventory, (Apple a) -> a.getWeight() > 150);
+		System.out.println(heavyApples2);
+
+		// []
+		List<Apple> weirdApples = filterApples(inventory,
+				(Apple a) -> a.getWeight() < 80 || "brown".equals(a.getColor()));
+		System.out.println(weirdApples);
+	}
+
+	public static boolean isGreenApple(Apple apple) {
+		return "green".equalsIgnoreCase(apple.getColor());
+	}
+
+	public static boolean isHeavyApple(Apple apple) {
+		return apple.getWeight() > 150;
+	}
+
+	public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+		List<Apple> result = new ArrayList<>();
+		for (Apple apple : inventory) {
+			if (p.test(apple)) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
+}
